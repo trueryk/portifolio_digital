e1cfd413c95489a7a2dec69c5b3d8dec4e473d1a
trueryk <henrique.jf01@gmail.com>
2026-08-13 09:04:49 -0300

    v03 portifolio

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,12 +4,14 @@ export default function Menu() {
 
     return (
         <>
-                <ul className="fixed flex flex-row h-[4vh] ms-4 gap-4 content-center">
+            <div className="fixed py-[3vh] align-center">
+                <ul className="flex flex-row ms-4 gap-4">
                     <li><a href="#sobre-curso">Sobre o curso</a></li>
                     <li> 2 </li>
                     <li> 3 </li>
                     <li> 4 </li>
                 </ul>
+            </div>
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,34 @@
+import fatecid from '../../public/fatecid.png'
 
 export default function Home() {
     return (
         <>
             <section className="bg-(--bg-blue) pt-16 font-bold h-screen" id="inicio">
-                <div className="flex flex-row   ">
-                    <h1 className="ms-16 text-start text-[5rem] text-(--text-blue)">
-                        Portifólio
-                        <span className="block -mt-10">Digital</span>
-                    </h1>
-                    <div className="">
-                        <p className="bottom-0 inset-x-0 -ms-[8vw] text-(--text-blue)">AAAAAAA</p>
+                <div className="grid grid-row-2">
+                    <div className="flex flex-row">
+                        <h1 className="ms-16 text-start text-[5rem] text-(--text-blue)">
+                            Portifólio
+                            <span className="block -mt-10">Digital
+                                <p className="inline text-(--text-blue) text-[1.2rem] ms-4 font-normal">Uma visita ao meu portfólio digital.</p>
+                            </span>
+                        </h1>
+
+                    </div>
+                    <div className="h-[50vh] w-[30vw] flex justify-center justify-self-center text-justify">
+                        <p className="inline-block self-end font-normal text-(--text-blue) text-[1.4rem]">Conheça um pouco do trabalho desenvolvido durante o 2º semestre de 2026</p>
                     </div>
                 </div>
             </section>
-            <section className="h-screen" id="sobre-curso">
+            <section className="h-screen flex" id="sobre-curso">
+                <div className="grid grid-cols-2 gap-2 w-full">
+                    <div className="">
+                        <h1>Sobre o curso</h1>
 
+                    </div>
+                    <div className="h-full flex">
+                        <img className="w-[45vw] justify-self-center self-center" src={fatecid}/>
+                    </div>
+                </div>
             </section>
         </>
     )
