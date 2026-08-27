dd02d0f3f396fd82aa3c9d3e9d24d84ba1f1e353
trueryk <henrique.jf01@gmail.com>
2026-08-27 09:12:05 -0300

    att

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import fatecid from '../../public/fatecid.png'
 export default function Home() {
     return (
         <>
-            <section className="bg-(--bg-blue) pt-16 font-bold h-screen" id="inicio">
+            <section className=" pt-16 font-bold h-screen" id="inicio">
                 <div className="grid grid-row-2">
                     <div className="flex flex-row">
                         <h1 className="ms-16 text-start text-[5rem] text-(--text-blue)">
@@ -20,13 +20,20 @@ export default function Home() {
                 </div>
             </section>
             <section className="h-screen flex" id="sobre-curso">
-                <div className="grid grid-cols-2 gap-2 w-full">
-                    <div className="">
-                        <h1>Sobre o curso</h1>
+                <div className="grid grid-cols-3 gap-2 w-full">
+                    <div className="col-span-1">
+                        <h1 className='text-center mt-15 mb-15 font-bold text-[3rem] text-(--text-blue)'>Sobre o curso</h1>
+                        <ul className='ms-15'>
+                            <li><span className='text-(--text-blue) font-bold'>Duração:</span> 3 Anos - 6 Semestres</li>
+                            <li><span className='text-(--text-blue) font-bold'>Formação:</span> Tecnólogo</li>
+                            <li><span className='text-(--text-blue) font-bold'>Presencial,</span> no horário da manhã, com 6º semestre Híbrido</li>
+                            <li><span className='text-(--text-blue) font-bold'>Cordenador:</span> Prof. Wellington Roque</li>
+                            <li className='mt-15'>Este curso pioneiro oferece uma abordagem abrangente para a construção de software que funciona em diversas plataformas!</li>
+                        </ul>
 
                     </div>
-                    <div className="h-full flex">
-                        <img className="w-[45vw] justify-self-center self-center" src={fatecid}/>
+                    <div className="col-span-2 h-full flex justify-center">
+                        <img className="w-[45vw]" src={fatecid} />
                     </div>
                 </div>
             </section>
